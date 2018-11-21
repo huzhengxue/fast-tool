bf61320c6b2e3363a6fa63ef45ab2ec59ff5cf58
xuwenping <wenpingxu123@163.com>
2018-11-21 23:23:22 +0800

    adjustment common, system-manage

---
+++ common/src/main/java/com/watermelon/scaffold/filter/WebRequestRecoderInterceptor.java
@@ -0,0 +1,29 @@
+package com.watermelon.scaffold.filter;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class WebRequestRecoderInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String url = request.getRequestURI();
+        // TODO add log
+        System.out.println(url);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
